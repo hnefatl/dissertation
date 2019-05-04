808db692360d453e41d3dd46b5f1c601125cf99f
Keith <hnefatl@gmail.com>
2019-05-04 21:11:58 +0100

    Updated wordcount with total count

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3.7
 
 import subprocess
+import termcolor
 
 sections = {
     "Intro": ("introduction.tex", 500, 0.05),
     "Prep": ("preparation.tex", 2500, 0.20),
-    "Impl": ("implementation.tex", 4500, 0.40),
+    "Impl": ("implementation.tex", 6500, 0.40),
     "Eval": ("evaluation.tex", 2000, 0.16),
-    "Conc": ("conclusion.tex", 500, 0.04)
+    "Conc": ("conclusion.tex", 500, 0.04),
 }
 
+total_wordcount = 0
 for section, (path, target, percentage) in sections.items():
-    wordcount = subprocess.check_output(f"texcount {path} | head -n3 | tail -n1 | awk '{{print $4}}'", shell=True).decode().strip()
-    print(f"{section.ljust(5)}:\t{wordcount.rjust(5)}/{str(target).rjust(4)}\t({percentage * 100}%)")
+    wordcount = (
+        subprocess.check_output(f"texcount {path} | head -n3 | tail -n1 | awk '{{print $4}}'", shell=True)
+        .decode()
+        .strip()
+    )
+    total_wordcount += int(wordcount)
+    print(f"{section.ljust(5)}:\t{wordcount.rjust(5)}/{str(target).rjust(5)}\t({percentage * 100}%)")
+
+target_total_wordcount = 12000
+okay_string = ""
+if total_wordcount < target_total_wordcount:
+    okay_string = termcolor.colored("✓", "green")
+else:
+    okay_string = termcolor.colored("❌", "red")
+print(f"Total:\t{total_wordcount}/{target_total_wordcount}\t{okay_string}")
